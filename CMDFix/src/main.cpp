00b4bb998991da3507739b059f0053c22d767865
JeppeSRC <jeppe97rc@gmail.com>
2018-11-22 18:49:07 +0100

    Added code and project

---
+++ CMDFix/src/main.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <sddl.h>
+
+DWORD id = 0x639bc8;
+
+bool err = false;
+
+void Fix(const char* keyPath, bool extended) {
+	HKEY key;
+
+	long res = RegOpenKeyEx(HKEY_CLASSES_ROOT, keyPath, 0, KEY_ALL_ACCESS, &key);
+
+	if (res != ERROR_SUCCESS) {
+		printf("Failed to open key! Admin privileges required");
+		err = true;
+		return;
+	}
+
+	RegDeleteKeyValue(key, "cmd", "HideBasedOnVelocityId");
+	RegSetKeyValue(key, "cmd", "ShowBasedOnVelocityId", REG_DWORD, &id, sizeof(DWORD));
+
+	if (extended) RegSetKeyValue(key, "cmd", "Extended", REG_SZ, 0, 0);
+	else RegDeleteKeyValue(key, "cmd", "Extended");
+
+	RegDeleteKeyValue(key, "Powershell", "ShowBasedOnVelocityId");
+	RegSetKeyValue(key, "Powershell", "HideBasedOnVelocityId", REG_DWORD, &id, sizeof(DWORD));
+
+	RegCloseKey(key);
+}
+
+int main(int argc, char** argv) {
+
+	bool extend = true;
+
+	if (argc == 2) {
+		if (memcmp(argv[1], "--no-extend", 11) == 0) extend = false;
+	}
+
+	Fix("\\Directory\\Background\\shell", extend);
+	Fix("\\Directory\\shell", extend);
+	Fix("\\Drive\\shell", extend);
+
+	if (!err) printf("Success!\n");
+	else printf("Failed!\n");
+
+	return 0;
+}
